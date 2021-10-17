856f578599a6d70890665eee084372797d536ee3
capucineleroy <91596703+capucineleroy@users.noreply.github.com>
2021-10-17 18:46:17 +0200

    Create rush_02.js

---
+++ rush_02/rush_02.js
@@ -0,0 +1,44 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+var widgetSrc = "https://widgets.booked.net/weather/info?action=get_weather_info;ver=7;cityID=876;type=11;scode=124;ltid=3458;domid=581;anc_id=84516;countday=undefined;cmetric=1;wlangID=3;color=ffffff;wwidth=118;header_color=2373ca;text_color=333333;link_color=ffffff;border_form=0;footer_color=ffffff;footer_text_color=333333;transparent=0;v=0.0.1";
+widgetSrc += ';ref=' + widgetUrl;
+widgetSrc += ';rand_id=97589';
+var weatherBookedScript = document.createElement("script");
+weatherBookedScript.setAttribute("type", "text/javascript");
+weatherBookedScript.src = widgetSrc;
+document.body.appendChild(weatherBookedScript)
+
+var css_file = document.createElement("link");
+var widgetUrl = location.href;
+css_file.setAttribute("rel", "stylesheet");
+css_file.setAttribute("type", "text/css");
+css_file.setAttribute("href", 'https://s.bookcdn.com/css/w/bw-120-100.css?v=0.0.1');
+document.getElementsByTagName("head")[0].appendChild(css_file);
+
+function setWidgetData_97589(data) {
+    if (typeof(data) != 'undefined' && data.results.length > 0) {
+        for (var i = 0; i < data.results.length; ++i) {
+            var objMainBlock = document.getElementById('m-booked-small-t1-97589');
+            if (objMainBlock !== null) {
+                var copyBlock = document.getElementById('m-bookew-weather-copy-' + data.results[i].widget_type);
+                objMainBlock.innerHTML = data.results[i].html_code;
+                if (copyBlock !== null) objMainBlock.appendChild(copyBlock);
+            }
+        }
+    } else {
+        alert('data=undefined||data.results is empty');
+    }
+}
